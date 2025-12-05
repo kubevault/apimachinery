06cfcef17cd9750bbe0f3ceb737352e6eeb7b45b
Rudro-25 <rudro@appscode.com>
2025-12-05 18:38:28 +0600

    temp
    Signed-off-by: Rudro-25 <rudro@appscode.com>

--- client/clientset/versioned/typed/engine/v1alpha1/engine_client.go
+++ client/clientset/versioned/typed/engine/v1alpha1/engine_client.go
@@ -21,10 +21,9 @@ package v1alpha1
 import (
 	"net/http"
 
+	rest "k8s.io/client-go/rest"
 	v1alpha1 "kubevault.dev/apimachinery/apis/engine/v1alpha1"
 	"kubevault.dev/apimachinery/client/clientset/versioned/scheme"
-
-	rest "k8s.io/client-go/rest"
 )
 
 type EngineV1alpha1Interface interface {
